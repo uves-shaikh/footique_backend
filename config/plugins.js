6740d6ad1ac6b7458391c5ba2b90990383cfda2b
uves-shaikh <suves58@gmail.com>
2023-03-30 02:28:07 +0530

    heroku try

--- config/plugins.js
+++ config/plugins.js
@@ -8,10 +8,6 @@ module.exports = ({ env }) => ({
           api_key: env('CLOUDINARY_KEY'),
           api_secret: env('CLOUDINARY_SECRET'),
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
       },
     },
     // ...
